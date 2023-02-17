0392a737bb67e2ab4072d3c109e62c9e2b1286a4
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-02-17 19:03:32 +0430

    Make dynamic the mobile and desktop view.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,102 @@
-function test() {
-  return 1 + 2;
+const devCarts = document.getElementById('dev-carts');
+
+const cartsData = [
+  {
+    id: '1',
+    name: 'Abdul Ali Noori',
+    title: 'Full-Stack Developer',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    img: 'images/dev-abdulali.jpg',
+  },
+  {
+    id: '2',
+    name: 'Ali Akbar Sazish',
+    title: 'Backed Developer',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    img: 'images/dev-sazish.jpg',
+  },
+  {
+    id: '3',
+    name: 'Ali Jan Rahimi',
+    title: 'Frontend Developer',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    img: 'images/dev-abdulali.jpg',
+  },
+  {
+    id: '4',
+    name: 'Abdul Ali Noori',
+    title: 'Full-Stack Developer',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    img: 'images/dev-abdulali.jpg',
+  },
+  {
+    id: '5',
+    name: 'Ali Akbar Sazish',
+    title: 'Backed Developer',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    img: 'images/dev-sazish.jpg',
+  },
+  {
+    id: '6',
+    name: 'Ali Jan Rahimi',
+    title: 'Frontend Developer',
+    bio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas alias omnis non consequuntur.',
+    img: 'images/dev-abdulali.jpg',
+  },
+];
+
+function generatProjectCarts() {
+  devCarts.innerHTML = cartsData.map((x) => {
+    const {
+      id, name, title, bio, img,
+    } = x;
+    if (id > 2) {
+      return `
+      <div class="dev-cart hidden" id="${id}">
+        <span>
+            <img src="${img}" width="100" alt="Developer image" class="dev-imge">
+        </span>
+        <div class="dev-details">
+            <h3 class="dev-name">${name}</h3>
+            <span class="dev-title">${title}</span>
+            <p class="dev-bio">${bio}</p>
+          </div>
+        </div>
+      `;
+    }
+    return `
+      <div class="dev-cart" id="${id}">
+        <span>
+            <img src="${img}" width="100" alt="Developer image" class="dev-imge">
+        </span>
+        <div class="dev-details">
+            <h3 class="dev-name">${name}</h3>
+            <span class="dev-title">${title}</span>
+            <p class="dev-bio">${bio}</p>
+          </div>
+        </div>
+        `;
+  }).join('');
 }
 
-test();
+generatProjectCarts();
+
+// ----------------------------less and more toggle-------------------------------------
+
+// --------------------------------toggle menu-------------------------------------------
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navBranding = document.querySelector('.nav-branding');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  navBranding.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link')
+  .forEach((links) => links.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
